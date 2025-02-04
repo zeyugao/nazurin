f932b55c48bf1505165613b427afed13824fbc76
netmaker <netmaker@lllp.com>
2025-02-04 23:47:29 +0800

    fix: douyin patterns

--- nazurin/sites/douyin/interface.py
+++ nazurin/sites/douyin/interface.py
@@ -8,7 +8,8 @@ from .config import COLLECTION
 
 patterns = [
     # 3.30 09/23 J@v.fo PKJ:/ 宝宝你是一个白巧小蛋糕 # cos # 碧蓝航线 # 可畏  https://v.douyin.com/ifwEwmBg/ 复制此链接，打开Dou音搜索，直接观看视频！
-    r"(.*\.douyin\.com/.*/)",
+    # https://www.douyin.com/video/7465981563767901498
+    r"(.*\.douyin\.com/.*)",
 ]
 
 
