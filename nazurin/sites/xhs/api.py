17536257d1540ae4ba3a399b0b86d9d982256147
Your Name <you@example.com>
2025-03-11 11:56:21 +0800

    feat: xhs

--- nazurin/sites/xhs/api.py
+++ nazurin/sites/xhs/api.py
@@ -1,7 +1,3 @@
-import os
-
-from urllib.parse import urlparse, urlunparse
-from datetime import datetime, timezone
 from typing import List, Tuple
 
 from nazurin.models import Caption, Illust, Image, File, Ugoira
@@ -11,108 +7,58 @@ from nazurin.utils.exceptions import NazurinError
 from nazurin.utils.logging import logger
 
 from .config import DESTINATION, FILENAME
-
-ERROR_NOT_FOUND = 4101147
+from .api import XhsApi
 
 
 class Xhs:
-    CONTENT_TYPE_MAP = {
-        "image/png": "png",
-        "image/jpeg": "jpeg",
-        "image/webp": "webp",
-        "video/mp4": "mp4",
-        "video/quicktime": "mov",
-        "audio/mp4": "m4a",
-        "audio/mpeg": "mp3",
-    }
 
-    async def get_dynamic(self, dynamic_id: str):
-        """
-            Get dynamic data from API.
-            Use API : https://github.com/Evil0ctal/Douyin_TikTok_Download_API
-        """
-        api = (
-            f"http://127.0.0.1:8000/xhs/"
-        )
-
-        async with Request() as request, request.get(api) as response:
-            response.raise_for_status()
-            data = await response.json()
-            # For some IDs, the API returns code 0 but empty content
-            logger.info(data)
-            msg = data.get("message")
-            if msg != '获取小红书作品数据成功' or not data.get('data',False):
-                raise NazurinError(
-                    f"Failed to get dynamic: msg = {msg}, "
-                    f"message = {data}",
-                )
-        return data["data"]
-        # return self.cleanup_item(item)
+    def __init__(self):
+        self.api = XhsApi()
 
     async def fetch(self, dynamic_id: str) -> Illust:
         """Fetch images and detail."""
-        
-        data = await self.get_dynamic(dynamic_id)
-        self.dynamic_id = data['作品ID']
+        self.url = dynamic_id
+        data = await self.api.get_dynamic(dynamic_id)
+        logger.info(data)
 
         # 判断链接类型/Judge link type
-        url_type = data["作品类型"]
-        caption = self.build_caption(data)
-        if url_type == '视频':
+        url_type = data["type"]
+        caption = self.build_caption(self.url, data)
+        if url_type == 'video':
             return await self.get_video(data)
-        elif url_type == '图文':
+        elif url_type == 'normal':
             return await self.get_images(data)
         return caption
 
-    async def get_video_format(self, url) -> str:
-        async with Request() as request, request.get(url) as response:
-            response.raise_for_status()
-            content_type = await response.headers.get("Content-Type")
-            format = Xhs.CONTENT_TYPE_MAP.get(content_type,'mp4')
-            return format
         
     async def get_video(self, data: dict) -> Ugoira:
         # 将信息储存在字典中/Store information in a dictionary
-        url = data.get('下载地址',None)[0]
-        if not url:
+        video_data = self.api.get_video_info(data)
+        if not video_data:
             raise NazurinError("No video found")
         
-        video_format = await self.get_video_format(url)
-
-        destination, filename = self.get_storage_dest(self.dynamic_id, data, video_format)
+        url = video_data['masterUrl']
+        video_format = video_data['format']
+        format = '.{}'.format(video_format)
+        destination, filename = self.get_storage_dest(data, format)
         file = File(filename, url, destination)
         async with Request() as session:
             await file.download(session)
-        return Ugoira(self.dynamic_id, file, self.build_caption(data), data)
+        return Ugoira(data['noteId'], file, self.build_caption(self.url, data), data)
     
-    async def get_image_info(self, url) -> dict:
-        async with Request() as request, request.get(url) as response:
-            response.raise_for_status()
-            data = await response.json()
-            # {"format":"png","width":"1240","height":"1664","size":"2243009","frame_count":"1","bit_depth":"8","md5":"56024e58bff75198e03ed11bc938c3f4","crc32":"4165107902","mime_type":"image/png","colorspace":"srgb"}
-        return data
-
     async def get_images(self, data: dict) -> List[Image]:
         """Get all images in a dynamic card."""
         
 
-        images = data.get('下载地址',None)
+        images = data.get('imageList',None)
         if not images or len(images) == 0:
             raise NazurinError("No image found")
     
         imgs = []
         for index, pic in enumerate(images):
-            url = pic
-            
-            parsed_url = urlparse(url)
-            # 修改查询字符串
-            media_info_url = urlunparse(parsed_url._replace(query="imageInfo"))
-            media_info = self.get_image_info(media_info_url)
-
-            extension = media_info['format']
-            # filename = os.basename(parsed_url.path)
-
-            destination, filename = self.get_storage_dest(self.dynamic_id, data, extension, index)
+            url = pic['urlDefault']
+            extension = '.jpg'
+            destination, filename = self.get_storage_dest( data, extension, index)
  
             size = None
             imgs.append(
@@ -120,35 +66,31 @@ class Xhs:
                     filename,
                     url,
                     destination,
-                    self.dynamic_id,
+                    data['noteId'],
                     size,
-                    media_info["width"],
-                    media_info["height"],
+                    data['width'],
+                    data['height'],
                 ),
             )
-        return Illust(int(self.dynamic_id), imgs, self.build_caption(data), data)
+        return Illust(data['noteId'], imgs, self.build_caption(self.url, data), data)
 
     @staticmethod
-    def get_storage_dest(filename:str, data: dict, file_format, index: int = 0) -> Tuple[str, str]:
+    def get_storage_dest(data: dict, file_format, index: int = 0) -> Tuple[str, str]:
         """
         Format destination and filename.
         """
+        timestamp = int(data['time'] / 1000)
 
-        timestamp = datetime.fromtimestamp(
-            data["发布时间"],
-            tz=timezone.utc,
-        )
-        # filename, extension = os.path.splitext(filename)
-        user = data["作者ID"]
+        user = data["user"]["nickname"]
         context = {
             **data,
             "user": user,
             # Original filename, without extension
             "index": index,
-            "filename": filename,
+            "filename": data['noteId'],
             "timestamp": timestamp,
             "extension": file_format,
-            "id_str": data["作品ID"],
+            "id_str": data['noteId'],
         }
         return (
             DESTINATION.format_map(context),
@@ -156,11 +98,11 @@ class Xhs:
         )
 
     @staticmethod
-    def build_caption(data: dict) -> Caption:
+    def build_caption(url, data: dict) -> Caption:
         return Caption(
             {
-                "author": "#" + data['作者昵称'],
-                "title": data["作品标题"],
-                "url": data["作品链接"]
+                "author": "#" + data["user"]["nickname"],
+                "title": data["title"],
+                "url": url
             },
         )
---
+++ nazurin/sites/xhs/api/converter.py
@@ -0,0 +1,66 @@
+from typing import Union
+
+from lxml.etree import HTML
+from yaml import safe_load
+
+__all__ = ["Converter"]
+
+
+class Converter:
+    INITIAL_STATE = "//script/text()"
+    KEYS_LINK = (
+        "note",
+        "noteDetailMap",
+        "[-1]",
+        "note",
+    )
+
+    def run(self, content: str) -> dict:
+        return self._filter_object(
+            self._convert_object(
+                self._extract_object(content)))
+
+    def _extract_object(self, html: str) -> str:
+        if not html:
+            return ""
+        html_tree = HTML(html)
+        scripts = html_tree.xpath(self.INITIAL_STATE)
+        return self.get_script(scripts)
+
+    @staticmethod
+    def _convert_object(text: str) -> dict:
+        return safe_load(text.lstrip("window.__INITIAL_STATE__="))
+
+    @classmethod
+    def _filter_object(cls, data: dict) -> dict:
+        return cls.deep_get(data, cls.KEYS_LINK) or {}
+
+    @classmethod
+    def deep_get(cls, data: dict, keys: list | tuple, default=None):
+        if not data:
+            return default
+        try:
+            for key in keys:
+                if key.startswith("[") and key.endswith("]"):
+                    data = cls.safe_get(data, int(key[1:-1]))
+                else:
+                    data = data[key]
+            return data
+        except (KeyError, IndexError, ValueError, TypeError):
+            return default
+
+    @staticmethod
+    def safe_get(data: Union[dict, list, tuple, set], index: int):
+        if isinstance(data, dict):
+            return list(data.values())[index]
+        elif isinstance(data, list | tuple | set):
+            return data[index]
+        raise TypeError
+
+    @staticmethod
+    def get_script(scripts: list) -> str:
+        scripts.reverse()
+        for script in scripts:
+            if script.startswith("window.__INITIAL_STATE__"):
+                return script
+        return ""
